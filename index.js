2e26fff8d210b56a7bd369ea6459919645514ef8
Margin Efim <pro100efim@gmail.com>
2020-07-14 23:16:33 +0500

    first bug fix (buttons, form, alt)

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const container = document.querySelector(".page");
-const editForm = document.querySelector(".edit-form"); 
+const editForm = document.querySelector(".form"); 
 const editButton = container.querySelector(".profile__edit-button");
-const closeButton = container.querySelector(".edit-form__close-button");
-const saveButton = container.querySelector(".edit-form__save-button");
+const closeButton = container.querySelector(".form__close-button");
 const titleInput = document.querySelector(".input__type_title");
 const infoInput = document.querySelector(".input__type_info");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
-const formElement = container.querySelector(".edit-form ");
+
 
 function editInfo(){    
 
     editForm.classList.remove("disabled");
-    console.log('reer');
     titleInput.setAttribute("value", profileTitle.textContent);
     infoInput.setAttribute("value", profileSubtitle.textContent);
 }
@@ -34,4 +32,4 @@ function formSubmitHandler(evt){
 
 editButton.addEventListener("click", editInfo);
 closeButton.addEventListener("click", closeBtnClick);
-formElement.addEventListener("submit", formSubmitHandler)
+editForm.addEventListener("submit", formSubmitHandler);
