d08d01293825d63cfece2561e2c92712ecbeb4e3
Margin Efim <pro100efim@gmail.com>
2020-08-03 18:27:38 +0500

    big bug fix

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const container = document.querySelector(".page");
 
 const editForm = document.querySelector(".edit_form"); 
 const createForm = document.querySelector(".create_form");
-const popup = document.querySelector(".popup");
+
 
 const editButton = container.querySelector(".profile__edit-button");
 const addButton = container.querySelector(".profile__add-button");
 
-const closeEditButton = container.querySelector('[aria-label="Close edit form"]');
-const closeAddButton = container.querySelector('[aria-label="Close add form"]');
+const closeEditButton = container.querySelector('.close_edit-form');
+const closeAddButton = container.querySelector('.close_add-form');
 const closePopupButton = container.querySelector(".popup__close-button");
 
-
-const saveButton = container.querySelector(".form__save-button");
-const createNewCardButton = container.querySelector(".form__create-button");
-const imageButton = container.querySelector(".photo__button-image");
 
 const placeNameInput = document.querySelector(".form__input_type_place-name");
 const infoInput = document.querySelector(".form__input_type_info");
 
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
-const formTitle = document.querySelector(".form__title");
+
+const popup = container.querySelector(".popup");
+const popupImage = container.querySelector(".popup__image");
+const popupTitle =  container.querySelector(".popup__title");
+
 
 const initialCards = [
     {
@@ -64,9 +64,10 @@ function openAddForm(){
 }
 
 function showImage(image, title){    
-    container.querySelector(".popup").classList.remove("disabled");
-    container.querySelector(".popup__image").src = image;
-    container.querySelector(".popup__title").textContent = title;
+    popup.classList.remove("disabled");
+    popupImage.src = image;
+    popupImage.alt  = title;
+    popupTitle.textContent = title;
 }
 
 function closeBtnClick(){     
@@ -82,16 +83,16 @@ function addNewCard(title, link){
     
     cardElement.querySelector(".photo__title").textContent = title;
     cardElement.querySelector(".photo__image").src = link;
-    cardElement.querySelector(".photo__button").addEventListener("click", function(evt){            
+    cardElement.querySelector(".photo__image").alt = title;
+    cardElement.querySelector(".photo__button").addEventListener("click", (evt) => {            
         evt.target.classList.toggle("photo__button_active");
     });
-    cardElement.querySelector(".photo__remove").addEventListener("click", function(){
-        this.parentNode.remove();
-        
-        // document.querySelector(".photo__element").remove();
+    cardElement.querySelector(".photo__remove").addEventListener("click", function(evt){
+        evt.target.parentNode.remove();        
+
     });
 
-    cardElement.querySelector(".photo__button-image").addEventListener("click", function(item){       
+    cardElement.querySelector(".photo__button-image").addEventListener("click", (item) =>{       
         
         showImage(link, title);
     });
@@ -99,32 +100,33 @@ function addNewCard(title, link){
     document.querySelector(".photo__elements").prepend(cardElement); 
 }
 
-function formSubmitHandler(evt){
-    evt.preventDefault();    
-    if(evt.target.classList[1] === "edit_form"){
-        profileTitle.textContent = placeNameInput.value;
-        profileSubtitle.textContent = infoInput.value;
-    }
-    else{
-        const newCardTitle = document.querySelector(".form__input_type_title");
-        const newCardLink = document.querySelector(".form__input_type_link");
-        addNewCard(newCardTitle.value, newCardLink.value);  
-        }   
-    
+function formSubmitHandlerEdit(evt){
+    evt.preventDefault();  
+    profileTitle.textContent = placeNameInput.value;
+    profileSubtitle.textContent = infoInput.value;
     closeBtnClick();   
 }
 
-initialCards.reverse().forEach(function(item){
+function formSubmitHandlerCreate(evt){
+    evt.preventDefault();   
+    const newCardTitle = document.querySelector(".form__input_type_title");
+    const newCardLink = document.querySelector(".form__input_type_link");
+    addNewCard(newCardTitle.value, newCardLink.value);
+    closeBtnClick(); 
+}
+
+const initialCardsReverse = Object.assign([], initialCards).reverse();
+
+initialCardsReverse.forEach(function(item){
     addNewCard(item.name, item.link);
  });
 
 editButton.addEventListener("click", editInfo);
 addButton.addEventListener("click", openAddForm);
-// imageButton.addEventListener("click", );
 
 closeEditButton.addEventListener("click", closeBtnClick);
 closeAddButton.addEventListener("click", closeBtnClick);
 closePopupButton.addEventListener("click", closeBtnClick);
 
-editForm.addEventListener("submit", formSubmitHandler);
-createForm.addEventListener("submit", formSubmitHandler);
+editForm.addEventListener("submit", formSubmitHandlerEdit);
+createForm.addEventListener("submit", formSubmitHandlerCreate);
